44ee62477257bc5ecf22d8bfff2670913765e59a
DianaBeta <dianabetancourt7@gmail.com>
2020-08-26 17:44:30 +0200

    styłe: images changed in home page

--- responsive-navbar.js
+++
@@ -1,9 +0,0 @@
-/* Adds responsive navbar */
-function myFunction() {
-  var x = document.getElementById("my-navbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
